4451f730e78ee80a48642981b46a695dec50c9e8
Chen Wei <weichen2046@gmail.com>
2017-09-12 07:03:41 +0800

    enterprise-repo: add form check before submit register page

--- enterprise-repo/frontend/src/app/app.routing.ts
+++ enterprise-repo/frontend/src/app/app.routing.ts
@@ -1,11 +1,11 @@
 import { HomePage } from './pages/homepage';
 import { LoginPage } from './pages/loginpage';
-import { RegisterPage } from './pages/registerpage';
+import { RegisterPageComponent } from './pages/registerpage';
 
 import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
     { path: '', component: HomePage, pathMatch: 'full' },
     { path: 'login', component: LoginPage, },
-    { path: 'register', component: RegisterPage, },
+    { path: 'register', component: RegisterPageComponent, },
 ]
--- enterprise-repo/frontend/src/app/pages/registerpage/registerpage.ts
+++ enterprise-repo/frontend/src/app/pages/registerpage/registerpage.ts
@@ -1,18 +1,57 @@
 import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MdButtonModule, MdInputModule } from '@angular/material';
 
 import { FooterModule } from '../../shared/footer';
+
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 @Component({
     selector: 'app-register',
     templateUrl: './registerpage.html',
     styleUrls: ['./registerpage.scss'],
 })
-export class RegisterPage { }
+export class RegisterPageComponent {
+    private pwdFormControl = new FormControl('', [
+        Validators.required,
+    ]);
+    private confirmpwdFormControl = new FormControl('', [
+        Validators.required,
+    ]);
+    private formGroup = new FormGroup({
+        'email': new FormControl('', [
+            Validators.required,
+            Validators.pattern(EMAIL_REGEX),
+        ]),
+        'username': new FormControl('', [
+            Validators.required,
+        ]),
+        'password': this.pwdFormControl,
+        'confirmpwd': new FormControl('', [
+            Validators.required,
+            (c) => {
+                return this.pwdFormControl.value === c.value ? null : { 'mismatch': true };
+            }
+        ]),
+    });
+
+    register() {
+        // TODO:
+        console.log('register called');
+    }
+}
 
 @NgModule({
-    imports: [FooterModule, MdButtonModule, MdInputModule],
-    declarations: [RegisterPage],
-    exports: [RegisterPage],
+    imports: [
+        FooterModule,
+        MdButtonModule,
+        MdInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CommonModule,
+    ],
+    declarations: [RegisterPageComponent],
+    exports: [RegisterPageComponent],
 })
 export class RegisterModule { }
